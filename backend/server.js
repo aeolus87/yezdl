3b066b816657fb34435940a7f12db118cebd64d7
aeolus87 <callejo.julius.07222003@gmail.com>
2024-07-23 16:50:45 +0800

    6th

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const videoRoutes = require("./routes/videoRoutes");
-
-dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
